de619b02da409f95bc0d617b8843a21a5d714728
4342315yc <4342315yc@gmail.com>
2020-09-13 23:52:42 +1000

    Partial Positioning

--- scdc/agents/baseline_agents.py
+++ scdc/agents/baseline_agents.py
@@ -234,16 +234,128 @@ class Kiting():
     
 
 class Positioning():
-    # TODO: Implement positioning trick
-    pass
+    '''
+    hard-coded
+    '''
+    def __init__(self, n_agents, env):
+        assert env.map_name in {'bane_vs_bane', 'so_many_baneling', '2c_vs_64zg'}, "Not supported map"
+        self.n_agents = n_agents 
+        self.env = env
+        self.map_name = env.map_name
+        self.in_position = False
+        
+    def step(self, obs, state):
+        if self.in_position:
+            actions = self.find_close_k()
+        else:
+            self.in_position, actions = self.partition()                
+         
+        reward, terminated, _ = self.env.step(actions)
+        return reward, terminated    
+    
+    def partition(self):
+        if self.map_name == 'bane_vs_bane':
+            # 5 partitions
+            zerg_partition_counter = 1
+            banelings, zergs = [], []
+            actions = []
+            patition_done = True
+            
+            for agent_id in range(self.n_agents):
+                unit = self.env.get_unit_by_id(agent_id)
+                if unit.health > 0:
+                    if unit.unit_type == self.env.baneling_id:
+                        actions.append(1)
+                    else:
+                        # 20 zergs in total
+                        pos = unit.pos 
+                        if zerg_partition_counter <= 5: 
+                            move_direction = self.get_move_direction(pos, (7, 11), zerg_partition_counter)
+                        elif zerg_partition_counter <= 10: 
+                            move_direction = self.get_move_direction(pos, (10, 8), zerg_partition_counter)
+                        elif zerg_partition_counter <= 15: 
+                            move_direction = self.get_move_direction(pos, (21, 11), zerg_partition_counter)
+                        elif zerg_partition_counter <= 20: 
+                            move_direction = self.get_move_direction(pos, (24, 8), zerg_partition_counter)
+                            
+                        zerg_partition_counter += 1
+                        
+                        actions.append(move_direction)
+                        if actions[-1] != 1:
+                            patition_done = False
+                else:
+                    actions.append(1)
+                    
+            return patition_done, actions
+        
+        elif self.map_name == 'so_many_baneling':
+            pass
+        else:
+            pass
+    
+    def get_group_center(self, group):
+        center_x, center_y = 0, 0
+        for unit in group:
+            center_x += unit.pos.x 
+            center_y += unit.pos.y
+        return (center_x/len(group), center_y/len(group))
+        
+        
+    def get_move_direction(self, center, distination, zerg_partition_counter):
+        center_x, center_y = center.x, center.y
+        if self.env.distance(center_x, center_y, distination[0], distination[1]) < 3:
+            return 1
+        else:
+            
+            if zerg_partition_counter <= 5:
+                return 5
+            elif zerg_partition_counter <= 10:
+                if np.abs(distination[0]-center_x) > np.abs(distination[1]-center_y):
+                    action = 5
+                else:
+                    action = 3
+                    
+            elif zerg_partition_counter <= 15:
+                return 4
+                    
+                    
+            elif zerg_partition_counter <= 20:
+                if np.abs(distination[0]-center_x) > np.abs(distination[1]-center_y):
+                    action = 4
+                else:
+                    action = 3
 
-
+        return action
+    
+    def find_close_k(self):
+        e_id_arr = []
+        target_items = self.env.enemies.items()
+        for agent_id in range(self.n_agents):
+            
+            unit = self.env.get_unit_by_id(agent_id)
+            min_dist = math.hypot(self.env.max_distance_x, self.env.max_distance_y)
+            min_dist_id = -5
+            
+            for t_id, t_unit in target_items: # t_id starts from 0
+                if t_unit.health > 0:
+                    dist = self.env.distance(unit.pos.x, unit.pos.y, t_unit.pos.x, t_unit.pos.y)
+                    if dist < min_dist:
+                        min_dist = dist
+                        min_dist_id = t_id 
+            
+            e_id_arr.append(min_dist_id+6)
+        
+        return e_id_arr
+    
 class WallOff():
+    '''
+    hard-coded
+    '''
     def __init__(self, n_agents, env):
         assert env.map_name == 'corridor', "WallOff trick only works for corridor maps."
         self.n_agents = n_agents 
         self.env = env
-        self.destination_point = (9.5, 9.5) # this one is hard-coded 
+        self.destination_point = (9, 9) # this one is hard-coded 
         self.arrival = False 
         
     def step(self, obs, state):
@@ -276,7 +388,7 @@ class WallOff():
             
             unit = self.env.get_unit_by_id(agent_id)
             min_dist = math.hypot(self.env.max_distance_x, self.env.max_distance_y)
-            min_dist_id = None
+            min_dist_id = -5
             
             for t_id, t_unit in target_items: # t_id starts from 0
                 if t_unit.health > 0:
