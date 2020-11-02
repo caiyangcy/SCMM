1ace773b997b0bcfc8b3942be588b7295b41745d
4342315yc <4342315yc@gmail.com>
2020-11-02 22:18:00 +1100

    Removed report

--- report/code/hello.java
+++
@@ -1,7 +0,0 @@
-void main(String[] args)
-{
-  System.out.println("Hello World");
-  List<String> a = new ArrayList<String>(); (*@ \label{line:jbug-start} @*)
-  a.add("foo");
-  List<Object> b = a; // This is a compile-time error (*@ \label{line:jbug-end} @*)
-}
