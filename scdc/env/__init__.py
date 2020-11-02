bcbf9863237f0ab60579f55fed3277c78edb2e30
4342315yc <4342315yc@gmail.com>
2020-11-02 22:15:44 +1100

    Update

--- scdc/env/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from scdc.env.multiagentenv import MultiAgentEnv
-from scdc.env.micro_env.mm_env import MMEnv
-
-__all__ = ["MultiAgentEnv", "MMEnv"]
