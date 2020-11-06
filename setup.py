dd4c4afb8f01d96c459894a020eadb807498987d
4342315yc <4342315yc@gmail.com>
2020-11-06 16:08:02 +1100

    update on setup requirements

--- setup.py
+++ setup.py
@@ -37,9 +37,5 @@ setuptools.setup(
         'absl-py>=0.1.0',
         'numpy==1.19.3', # 1.19.4 has a bug causing failure on sanity check
         'matplotlib==3.2.2',
-        'torch'
-    ],
-    dependency_links=[
-      'https://download.pytorch.org/whl/torch_stable.html'
     ],
 )
